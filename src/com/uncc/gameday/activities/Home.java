188d6e0961da626cde2e65d7c02e5a3c3f7e00ad
bspeice <bspeice@uncc.edu>
2013-10-04 21:17:50 -0400

    Add some skeleton code for fetching alerts.

--- src/com/uncc/gameday/activities/Home.java
+++ src/com/uncc/gameday/activities/Home.java
@@ -1,7 +1,30 @@
 package com.uncc.gameday.activities;
 
+import java.util.List;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.uncc.gameday.R;
 
 public class Home extends MenuActivity {
-
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		
+		Context ctx = getApplicationContext();
+		
+	}
+	
+	private void getRecentAlerts() {
+		// Responsible for discovering what the most recent alerts are and showing them.
+		// Likely should be implemented by querying a local DB of alerts, grabbing recent 20.
+		new Alerts().fetchAlerts();
+	}
 }

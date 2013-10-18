a02e59b81fb366aa00f7a763297dd88052016570
bspeice <bspeice@uncc.edu>
2013-10-18 18:47:23 -0400

    Change ParkingRating to use the POJO pattern

--- src/com/uncc/gameday/registration/ParkingRating.java
+++ src/com/uncc/gameday/registration/ParkingRating.java
@@ -1,6 +1,19 @@
 package com.uncc.gameday.registration;
 
 public class ParkingRating {
-	ParkingChoices parking_lot;
-	RatingChoices rating;
+	private ParkingChoices parking_lot;
+	private RatingChoices rating;
+	
+	public ParkingChoices getParkingLot() {
+		return parking_lot;
+	}
+	public void setParkingLot(ParkingChoices parking_lot) {
+		this.parking_lot = parking_lot;
+	}
+	public RatingChoices getRating() {
+		return rating;
+	}
+	public void setRating(RatingChoices rating) {
+		this.rating = rating;
+	}
 }

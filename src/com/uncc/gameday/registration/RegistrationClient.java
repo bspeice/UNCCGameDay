d6586d3ac9c735bbc4e76fd5628dea5474ed9a9a
bspeice <bspeice@uncc.edu>
2013-10-18 17:46:37 -0400

    Add the new (and tested!) RegistrationClient

---
+++ src/com/uncc/gameday/registration/RegistrationClient.java
@@ -0,0 +1,32 @@
+package com.uncc.gameday.registration;
+
+import java.util.List;
+
+import com.uncc.gameday.R;
+
+import android.content.Context;
+import android.util.Log;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+
+public class RegistrationClient {
+	
+	private GamedayService gds;
+	
+	public RegistrationClient(Context c) {
+		RestAdapter ra = new RestAdapter.Builder()
+			.setServer("http://" + c.getString(R.string.server_hostname))
+			.build();
+		gds = ra.create(GamedayService.class);
+	}
+	
+	public List<ParkingLot> listLots() {
+		try {
+			return gds.listLots();
+		} catch (RetrofitError e) {
+			Log.w("Registration.listLots()", e);
+			return null;
+		}
+	}
+
+}

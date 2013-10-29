f6da66101d0ebe23e1edef03d0b2e34d0a5f8cb3
RowenaWinston <rwinsto6@uncc.edu>
2013-10-28 23:06:37 -0400

    UI commit2

--- src/com/uncc/gameday/activities/Parking.java
+++ src/com/uncc/gameday/activities/Parking.java
@@ -1,6 +1,15 @@
 package com.uncc.gameday.activities;
 
+import android.os.Bundle;
+
+import com.uncc.gameday.R;
+
 
 public class Parking extends MenuActivity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_parking);
+	}
 }

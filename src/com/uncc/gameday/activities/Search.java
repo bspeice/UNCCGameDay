05cf6078188c4710ce308c9b8e37190eff8961a7
bspeice <bspeice@uncc.edu>
2013-12-04 11:22:49 -0500

    Add the RSVP search activity

---
+++ src/com/uncc/gameday/activities/Search.java
@@ -0,0 +1,37 @@
+package com.uncc.gameday.activities;
+
+import java.util.List;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.uncc.gameday.R;
+import com.uncc.gameday.registration.Attendee;
+import com.uncc.gameday.registration.RegistrationClient;
+
+public class Search extends MenuActivity {
+	
+	List<Attendee> rsvpList;
+	boolean listFetched = false;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_rsvp);
+		new fetchAttendeesThread(this).start();
+	}
+	
+	private class fetchAttendeesThread extends Thread {
+		Context c;
+		public fetchAttendeesThread(Context c) {
+			this.c = c;
+		}
+		
+		public void run() {
+			RegistrationClient client = new RegistrationClient(this.c);
+			rsvpList = client.listAttendees();
+			listFetched = true;
+		}
+	}
+
+}
